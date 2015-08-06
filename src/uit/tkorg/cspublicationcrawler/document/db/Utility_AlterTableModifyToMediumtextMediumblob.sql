375f56ba23ed3e6bf84cfeee2342b20ab239ca43
Tran Hung Nghiep <tranhungnghiep@gmail.com>
2015-08-06 18:09:28 +0700

    Branch GUI ver. 150911

--- src/uit/tkorg/cspublicationcrawler/document/db/Utility_AlterTableModifyToMediumtextMediumblob.sql
+++
@@ -1,5 +0,0 @@
-alter table Paper modify abstract MEDIUMBLOB NULL DEFAULT NULL;
-alter table Paper modify viewPublication MEDIUMTEXT NULL DEFAULT NULL;
-alter table Comment modify content MEDIUMTEXT NULL DEFAULT NULL;
-alter table Paper_Reviewer modify content MEDIUMTEXT NULL DEFAULT NULL;
-alter table Paper_Paper modify citationContext MEDIUMTEXT NULL DEFAULT NULL;
